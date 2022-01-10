baca7230eae799f204f9b971c5aa4532b0044e3e
Eduard Bucari <eduard.bucari@gmail.com>
2022-01-10 03:20:26 +0300

    4. candicates.jsp - список кандидатов. [#276522]

--- src/main/java/ru/job4j/dreamjob/model/store/Store.java
+++ src/main/java/ru/job4j/dreamjob/model/store/Store.java
@@ -1,5 +1,6 @@
 package ru.job4j.dreamjob.model.store;
 
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.model.Post;
 
 import java.util.Calendar;
@@ -14,17 +15,26 @@ public class Store {
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "Junior", new Date(2021, Calendar.DECEMBER, 15)));
         posts.put(2, new Post(2, "Middle Java Job", "Middle", new Date(2021, Calendar.OCTOBER, 26)));
         posts.put(3, new Post(3, "Senior Java Job", "Senior", new Date(2021, Calendar.MAY, 30)));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }

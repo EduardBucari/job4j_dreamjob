f9dd7ebcbc5b54102f497e97163db2af8506e783
Eduard Bucari <eduard.bucari@gmail.com>
2022-01-17 01:55:14 +0300

    1. Загрузка и скачивание файла. [#154183 #250645](4)

--- src/main/java/ru/job4j/dreamjob/model/servlet/DownloadServlet.java
+++ src/main/java/ru/job4j/dreamjob/model/servlet/DownloadServlet.java
@@ -1,5 +1,7 @@
 package ru.job4j.dreamjob.model.servlet;
 
+import ru.job4j.dreamjob.model.Path;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +15,7 @@ public class DownloadServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         File downloadFile = null;
-        for (File file : new File("c:\\images\\").listFiles()) {
+        for (File file : new File(Path.candidatePic()).listFiles()) {
             if (name.equals(file.getName())) {
                 downloadFile = file;
                 break;

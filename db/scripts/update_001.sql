fe2ad830c2749b158f2996e08519c135c4d0f726
Eduard Bucari <eduard.bucari@gmail.com>
2022-01-17 01:42:08 +0300

    3. Тесты на DbStore [#504817](6)

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,16 +1,4 @@
-create TABLE if not exists post
-(
-    id          SERIAL PRIMARY KEY,
-    name        TEXT,
-    description TEXT,
-    create_date timestamp default localtimestamp
-);
-
-create TABLE if not exists candidate
-(
-    id          SERIAL PRIMARY KEY,
-    name        TEXT,
-    position    varchar(200),
-    create_date timestamp default localtimestamp,
-    city_id     int references city (id)
+CREATE TABLE post (
+    id SERIAL PRIMARY KEY,
+    name TEXT
 );

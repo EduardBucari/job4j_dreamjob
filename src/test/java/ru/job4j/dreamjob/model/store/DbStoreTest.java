833610ec9bc675b019a12b4f270bb0921795de93
Eduard Bucari <eduard.bucari@gmail.com>
2022-01-18 20:50:21 +0300

    3. Тесты на DbStore [#504817 #251453](12)

---
+++ src/test/java/ru/job4j/dreamjob/model/store/DbStoreTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.dreamjob.model.store;
+
+import org.junit.Test;
+import ru.job4j.dreamjob.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class DbStoreTest {
+    @Test
+    public void whenCreatePost() {
+        Store store = DbStore.instOf();
+        Post post = new Post(0, "Java Job");
+        store.save(post);
+        Post postInDb = store.findById(post.getId());
+        assertThat(postInDb.getName(), is(post.getName()));
+    }
+    @Test
+    public void whenUpdatePost() {
+        Store store = DbStore.instOf();
+        Post post = new Post(1, "Java Job");
+        Post post1 = new Post(1, "Java MidlMan");
+        store.save(post);
+        store.save(post1);
+        Post postInDb = store.findById(1);
+        assertThat(postInDb.getName(), is(post1.getName()));
+    }
+    @Test
+    public void whenFindAllPost() {
+        Store store = DbStore.instOf();
+        Post post = new Post(0, "Java Master");
+        Post post1 = new Post(1, "Java Developer");
+        store.save(post);
+        store.save(post1);
+        List<Post> posts =  new ArrayList<>(store.findAllPosts());
+        assertThat(posts.iterator().next().getName(), is(post1.getName()));
+    }
+}

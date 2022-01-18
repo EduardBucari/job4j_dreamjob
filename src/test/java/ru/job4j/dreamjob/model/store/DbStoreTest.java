16ebd728b9f6101009a1616c18684b1866cb232d
Eduard Bucari <eduard.bucari@gmail.com>
2022-01-18 20:08:05 +0300

    3. Тесты на DbStore [#504817 #251453](8)

--- src/test/java/ru/job4j/dreamjob/model/store/DbStoreTest.java
+++ src/test/java/ru/job4j/dreamjob/model/store/DbStoreTest.java
@@ -1,7 +1,10 @@
 package ru.job4j.dreamjob.model.store;
 
+
+import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.model.Candidate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,8 +12,10 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+
 public class DbStoreTest {
-    /**
+
+    @Ignore
     @Test
     public void whenCreatePost() {
         Store store = DbStore.instOf();
@@ -20,24 +25,64 @@ public class DbStoreTest {
         assertThat(postInDb.getName(), is(post.getName()));
     }
 
+    @Ignore
     @Test
     public void whenUpdatePost() {
         Store store = DbStore.instOf();
         Post post = new Post(1, "Java Job");
-        Post post1 = new Post(1, "Java Middle");
+        Post post1 = new Post(1, "Java MidlMan");
         store.save(post);
         store.save(post1);
         Post postInDb = store.findById(1);
         assertThat(postInDb.getName(), is(post1.getName()));
     }
 
+    @Ignore
     @Test
     public void whenFindAllPost() {
         Store store = DbStore.instOf();
-        Post post = new Post(1, "Java Job");
-        List<Post> posts =  new ArrayList<>(store.findAllPosts());
+        Post post = new Post(0, "Java Master");
+        Post post1 = new Post(1, "Java Developer");
         store.save(post);
-        assertThat(posts.get(posts.size() - 1).getName(), is(post.getName()));
+        store.save(post1);
+        List<Post> posts =  new ArrayList<>(store.findAllPosts());
+
+        assertThat(posts.iterator().next().getName(), is(post1.getName()));
     }
-    **/
+
+    @Ignore
+    @Test
+    public void whenCreateCandidate() {
+        Store store = DbStore.instOf();
+        Candidate candidate = new Candidate(0, "Java JobMan");
+        store.save(candidate);
+        Candidate cndInDb = store.findByIdCandidate(candidate.getId());
+        assertThat(cndInDb.getName(), is(candidate.getName()));
+    }
+
+    @Ignore
+    @Test
+    public void whenUpdateCandidate() {
+        Store store = DbStore.instOf();
+        Candidate cnd = new Candidate(1, "Java JobMan");
+        Candidate cnd1 = new Candidate(1, "Java MidlMan");
+        store.save(cnd);
+        store.save(cnd1);
+        Candidate cndInDb = store.findByIdCandidate(1);
+        assertThat(cndInDb.getName(), is(cnd1.getName()));
+    }
+
+    @Ignore
+    @Test
+    public void whenFindAllCandidate() {
+        Store store = DbStore.instOf();
+        Candidate cnd = new Candidate(0, "Java JobManMan");
+        Candidate cnd1 = new Candidate(1, "Java MidlMan");
+        store.save(cnd);
+        store.save(cnd1);
+        List<Candidate> list = new ArrayList<>(store.findAllCandidates());
+        assertThat(list.iterator().next().getName(), is(cnd1.getName()));
+    }
+
+
 }

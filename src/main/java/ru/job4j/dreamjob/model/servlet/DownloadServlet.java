b55bf6f20ee3e41ab8b8d628eb781fdab432843b
Eduard Bucari <eduard.bucari@gmail.com>
2022-01-22 03:01:41 +0300

    servlet changes(2)

--- src/main/java/ru/job4j/dreamjob/model/servlet/DownloadServlet.java
+++ src/main/java/ru/job4j/dreamjob/model/servlet/DownloadServlet.java
@@ -1,6 +1,6 @@
 package ru.job4j.dreamjob.model.servlet;
 
-import ru.job4j.dreamjob.model.Path;
+import ru.job4j.dreamjob.model.Config;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,17 +13,20 @@ import java.io.IOException;
 public class DownloadServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+        String name = req.getParameter("id");
         File downloadFile = null;
-        for (File file : new File(Path.candidatePic()).listFiles()) {
-            if (name.equals(file.getName())) {
+        System.out.println(name);
+        for (File file : new File(Config.getConfig().getProperty("path")).listFiles()) {
+            System.out.println(file.toString());
+            String[] arStr = file.getName().split("\\.");
+            if (name.equals(arStr[0])) {
                 downloadFile = file;
                 break;
             }
         }
         resp.setContentType("application/octet-stream");
-        resp.setHeader("Content-Disposition", "attachment; filename=\"" + downloadFile.getName() + "\"");
+        resp.setHeader("Content-Disposition", "attachment; filename=\"" + downloadFile  + "\"");
         try (FileInputStream stream = new FileInputStream(downloadFile)) {
             resp.getOutputStream().write(stream.readAllBytes());
         }
--- src/main/java/ru/job4j/dreamjob/model/servlet/UploadServlet.java
+++ src/main/java/ru/job4j/dreamjob/model/servlet/UploadServlet.java
@@ -4,7 +4,7 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import ru.job4j.dreamjob.model.Path;
+import ru.job4j.dreamjob.model.Config;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -19,39 +19,82 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class UploadServlet extends HttpServlet {
+/**
+ * Когда в браузере открывается любая ссылка, он отправляет http запрос с типом GET.
+ * Когда в браузере отправляется form нужно использовать метод doPost.
+ */
+public class UploadServlet  extends HttpServlet {
+
+    /**
+     * GET — обрабатывает адресную строку, которая получается при вызове сервлета.
+     * Например, site.com/example?action=test&id=10&admin=true
+     * Для того, чтобы выполнить перенаправление запроса, вначале с помощью метода getServletContext()
+     * получаем объект ServletContext, который представляет контекст запроса.
+     * Затем с помощью его метода getRequestDispatcher() получаем объект RequestDispatcher.
+     * Путь к ресурсу, на который надо выполнить перенаправление, передается в качестве параметра в getRequestDispatcher.
+     * Затем у объекта RequestDispatcher вызывается метод forward(),
+     * в который передаются объекты HttpServletRequest и HttpServletResponse.
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<String> images = new ArrayList<>();
-        for (File name : Objects.requireNonNull(
-                new File(Path.candidatePic())
-        .listFiles())) {
-            images.add(name.getName());
-        }
-        req.setAttribute("images", images);
-        System.out.println("do get " + req.getParameter("id"));
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/upload.jsp");
+        RequestDispatcher dispatcher = req.getServletContext().getRequestDispatcher("/photoUpload.jsp");
         dispatcher.forward(req, resp);
     }
 
+    /**
+     * POST — обрабатывает загруженный контент (картинку, файл, строки, объектные данные),
+     * в общем, всё, что можно передать через HTTP
+     *
+     * здесь выполняется создание фабрики парсинга входящих запросов DiskFileItemFactory.
+     *
+     * Объект ServletContext является уникальным и доступен всем сервлетам веб приложения.
+     * Мы можем использовать объект ServletContext, когда нам необходимо предоставить
+     * доступ одному или нескольким сервлетам к инициализированным параметрам веб приложения.
+     *
+     * getServletContext().setAttribute("javax.servlet.context.tempdir","value");
+     * Путь к временному каталогу, который должен быть предоставлен этим контекстом для временного использования
+     * для чтения и записи сервлетами в соответствующем веб-приложении. Этот каталог станет видимым для сервлетов
+     * в веб-приложении с помощью атрибута контекста сервлета (типа java.io.File) с именем javax.servlet.context.tempdir,
+     * как описано в Спецификации сервлета. Если не указано иное, будет предоставлен подходящий каталог под $ CATALINA_HOME / work.
+     *
+     * setRepository()
+     * Устанавливает каталог, используемый для временного хранения файлов, размер которых превышает настроенный порог.
+     *
+     * ServletFileUpload Установить максимально допустимую загрузку файла
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String id = req.getParameter("id");
-        System.out.println("doPost " + id);
+
         DiskFileItemFactory factory = new DiskFileItemFactory();
         ServletContext servletContext = this.getServletConfig().getServletContext();
         File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
         factory.setRepository(repository);
         ServletFileUpload upload = new ServletFileUpload(factory);
         try {
+            /**
+             *  Получаем список всех данных в запросе.
+             */
             List<FileItem> items = upload.parseRequest(req);
-            File folder = new File(Path.candidatePic());
+            /**
+             * Если элемент не поле, то это файл и из него можно прочитать весь входной
+             * поток и записать его в файл или напрямую в базу данных.
+             */
+            File folder = new File(Config.getConfig().getProperty("path"));
             if (!folder.exists()) {
                 folder.mkdir();
             }
             for (FileItem item : items) {
                 if (!item.isFormField()) {
-                    File file = new File(folder + File.separator + id + ".jpeg");
+
+                    File file = new File(folder + File.separator + req.getParameter("id") + ".png");
                     try (FileOutputStream out = new FileOutputStream(file)) {
                         out.write(item.getInputStream().readAllBytes());
                     }
@@ -60,6 +103,6 @@ public class UploadServlet extends HttpServlet {
         } catch (FileUploadException e) {
             e.printStackTrace();
         }
-        doGet(req, resp);
+        resp.sendRedirect(req.getContextPath() + "/candidates.do");
     }
 }
